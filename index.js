db22e779211c50b74ecd32f3c61b103bee51ce56
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-24 17:10:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,5 @@ function destructivelyUpdateObjectWithKeyAndValue (object, key, value) {
 function deleteFromObjectByKey (object, key) {
   var newObj =  Object.assign({},object)
   delete newObj [key]
-  
+  return newObj
 }

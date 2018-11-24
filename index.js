43859c5c69dd02333f2c4f96bcd188097f5cff76
Rachelle Ando <rachellelorraineandoart@gmail.com>
2018-11-24 17:06:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var recipes = new Object();
 
 
-function updateObjectWithKeyAndValue (object, key, value) {
-  return Object.assign ({}, object, {key: value})
+function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
 }
-
-function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
- object[key] = value
-return object
